ee44b8efd48ed64d466a55f4468d57c4c9f617f9
swilkiway <swilkiway@gmail.com>
2017-12-13 19:36:41 +0000

    orange

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,14 @@
 angular.module('myApp', ['firebase']).
   controller('dareController', ['$scope', '$http', '$firebaseArray', 
                               function($scope, $http, $firebaseArray) {
-    $http.get('/user/profile')
+var ref = firebase.database().ref().child("messages");
+   $scope.chats = $firebaseArray(ref);
+   $scope.update = function(user) {
+       var newmessage = {from:user.name || "anonymous",body:user.chat};
+       console.log(newmessage);
+       $scope.chats.$add(newmessage);
+       user.chat = "";   
+ $http.get('/user/profile')
         .success(function(data, status, headers, config) {
       $scope.user = data;
       $scope.error = "";
@@ -10,13 +17,6 @@ angular.module('myApp', ['firebase']).
       $scope.user = {};
       $scope.error = data;
     });
-var ref = firebase.database().ref().child("messages");
-   $scope.chats = $firebaseArray(ref);
-   $scope.update = function(user) {
-       var newmessage = {from:user.name || "anonymous",body:user.chat};
-       console.log(newmessage);
-       $scope.chats.$add(newmessage);
-       user.chat = "";
    }
   }
  ]);
