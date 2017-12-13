7e3a1b6491b3184734d4d346478a87f93b4352f9
swilkiway <swilkiway@gmail.com>
2017-12-13 17:17:11 +0000

    Changed more stuff

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,5 @@
-angular.module('myApp', []).
-  controller('dareController', ['$scope', '$http',
+angular.module('myApp', ["firebase"]).
+  controller('dareController', ['$scope', '$http', '$firebaseArray',
                               function($scope, $http) {
     $http.get('/user/profile')
         .success(function(data, status, headers, config) {
@@ -10,4 +10,14 @@ angular.module('myApp', []).
       $scope.user = {};
       $scope.error = data;
     });
-  }]);
+  },function($scope, $firebaseArray) {
+   var ref = firebase.database().ref().child("dares")
+   $scope.dare = $firebaseArray(ref);
+   $scope.update = function(user) {
+       var newmessage = {from:user.name || "anonymous",body:user.dare};
+       console.log(newmessage);
+       $scope.dare.$add(newmessage);
+       $scope.currentDare.setInnerHTML(newmessage);
+       user.dare = "";
+   }
+ }]);
