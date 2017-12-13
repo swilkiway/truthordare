16f362c4269e9301bc3ab764d2f35393c87a96c6
swilkiway <swilkiway@gmail.com>
2017-12-13 19:24:28 +0000

    Almost done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,6 @@
-angular.module('myApp', []).
-  controller('dareController', ['$scope', '$http', 
-                              function($scope, $http) {
+angular.module('myApp', ['firebase']).
+  controller('dareController', ['$scope', '$http', '$firebaseArray', 
+                              function($scope, $http, $firebaseArray) {
     $http.get('/user/profile')
         .success(function(data, status, headers, config) {
       $scope.user = data;
@@ -10,5 +10,13 @@ angular.module('myApp', []).
       $scope.user = {};
       $scope.error = data;
     });
+var ref = firebase.database().ref().child("messages");
+   $scope.chats = $firebaseArray(ref);
+   $scope.update = function(user) {
+       var newmessage = {from:user.name || "anonymous",body:user.chat};
+       console.log(newmessage);
+       $scope.chats.$add(newmessage);
+       user.chat = "";
+   }
   }
  ]);
